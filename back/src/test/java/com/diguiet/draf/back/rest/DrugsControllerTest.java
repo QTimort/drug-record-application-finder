5738b4685d6ccb97c032fa7e4add818fc995a10b
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-22 23:55:20 +0100

    Extend test

--- back/src/test/java/com/diguiet/draf/back/rest/DrugsControllerTest.java
+++ back/src/test/java/com/diguiet/draf/back/rest/DrugsControllerTest.java
@@ -2,13 +2,16 @@ package com.diguiet.draf.back.rest;
 
 
 import com.diguiet.draf.back.controllers.rest.DrugsController;
+import com.diguiet.draf.common.models.fda.DrugsFdaResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -46,11 +49,20 @@ public class DrugsControllerTest {
     }
 
     private void testDrugsEndpoint(String urlTemplate) throws Exception {
-        this.mockMvc
+        final MvcResult mvcResult = this.mockMvc
                 .perform(get(urlTemplate))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.meta.results.total").exists())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.results.products[0].brand_name").exists());
+                .andReturn();
+        final String json = mvcResult.getResponse().getContentAsString();
+        final DrugsFdaResponse parsedResponse = new ObjectMapper().readValue(json, DrugsFdaResponse.class);
+
+        Assertions.assertNotNull(parsedResponse);
+        Assertions.assertNotNull(parsedResponse.getResults());
+        Assertions.assertNotNull(parsedResponse.getMeta());
+        Assertions.assertEquals(
+                parsedResponse.getMeta().getResults().getTotal(),
+                parsedResponse.getResults().size()
+        );
     }
 }
