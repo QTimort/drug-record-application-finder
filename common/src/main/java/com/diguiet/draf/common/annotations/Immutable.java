5f6330674e45232bffd49dad1c7e4a227dc23c2e
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-22 06:11:17 +0100

    Add Immutable annotation

---
+++ common/src/main/java/com/diguiet/draf/common/annotations/Immutable.java
@@ -0,0 +1,9 @@
+package com.diguiet.draf.common.annotations;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Immutable {
+}
