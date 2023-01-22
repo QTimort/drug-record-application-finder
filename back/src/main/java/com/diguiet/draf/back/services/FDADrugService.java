86eb361241d04cf89389ac14d8694ff284487864
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-22 21:49:21 +0100

    Use soft match and filter products

--- back/src/main/java/com/diguiet/draf/back/services/FDADrugService.java
+++ back/src/main/java/com/diguiet/draf/back/services/FDADrugService.java
@@ -1,10 +1,16 @@
 package com.diguiet.draf.back.services;
 
 import com.diguiet.draf.common.models.fda.DrugsFdaResponse;
+import com.diguiet.draf.common.models.fda.Result;
 import lombok.Getter;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+
+import static java.util.stream.Collectors.toCollection;
+
 @Service
 public class FDADrugService {
     private static final String DEFAULT_FDA_DRUG_API_ENDPOINT = "https://api.fda.gov/drug/drugsfda.json";
@@ -26,7 +32,7 @@ public class FDADrugService {
             @NonNull final String manufacturer
     ) {
         return this.restTemplate.getForObject(
-                this.fdaDrugEndpoint + "?search=openfda.manufacturer_name.exact:\"{manufacturer}\"&limit=1000",
+                this.fdaDrugEndpoint + "?search=openfda.manufacturer_name:\"{manufacturer}\"&limit=1000",
                 DrugsFdaResponse.class,
                 manufacturer
         );
@@ -36,14 +42,35 @@ public class FDADrugService {
             @NonNull final String manufacturer,
             @NonNull final String brand
     ) {
-        return this.restTemplate.getForObject(
+        final DrugsFdaResponse drugsFdaResponse = this.restTemplate.getForObject(
                 this.fdaDrugEndpoint +
-                        "?search=openfda.manufacturer_name.exact:\"{manufacturer}\"," +
+                        "?search=openfda.manufacturer_name:\"{manufacturer}\"," +
                         "openfda.brand_name.exact:\"{brand}\"&limit=1000",
                 DrugsFdaResponse.class,
                 manufacturer,
                 brand
         );
+        if (drugsFdaResponse != null && drugsFdaResponse.getResults() != null) {
+            for (final Result result : drugsFdaResponse.getResults()) {
+                result.setProducts(
+                        result.getProducts()
+                                .stream()
+                                .filter(p -> p.getBrand_name().equals(brand))
+                                .collect(toCollection(ArrayList::new))
+                );
+            }
+            drugsFdaResponse.setResults(
+                    drugsFdaResponse.getResults()
+                            .stream()
+                            .filter(result -> !result.getProducts().isEmpty())
+                            .collect(toCollection(ArrayList::new))
+            );
+            drugsFdaResponse.getMeta().getResults().setTotal(
+                    drugsFdaResponse.getResults().size()
+            );
+        }
+
+        return drugsFdaResponse;
     }
 
 }
