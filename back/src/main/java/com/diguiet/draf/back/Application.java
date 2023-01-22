670ede88a205e748270fb0532d6f309874926a72
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-22 13:31:12 +0100

    Add swagger API Documentation

--- back/src/main/java/com/diguiet/draf/back/Application.java
+++ back/src/main/java/com/diguiet/draf/back/Application.java
@@ -3,6 +3,14 @@ package com.diguiet.draf.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(
+		title = "Drug Record Application Finder API",
+		version = "1.0",
+		description = "Search for drug record applications submitted to FDA for approval."
+))
 @SpringBootApplication
 public class Application {
 
--- back/src/main/java/com/diguiet/draf/back/controllers/rest/VersionController.java
+++ back/src/main/java/com/diguiet/draf/back/controllers/rest/VersionController.java
@@ -2,6 +2,11 @@ package com.diguiet.draf.back.controllers.rest;
 
 import com.diguiet.draf.back.properties.GitInfos;
 import com.diguiet.draf.common.models.BackVersion;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,6 +22,12 @@ public class VersionController {
         this.gitInfos = gitInfos;
     }
 
+    @Operation(summary = "Get Version")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get the  server version",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = BackVersion.class)) }),
+    })
     @CrossOrigin
     @GetMapping("/version")
     public BackVersion getVersion() {
