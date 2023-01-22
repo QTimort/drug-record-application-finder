e5b58ea7b4a0ad1a70fe2cd6df04ce33f005cdc9
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-22 04:23:10 +0100

    Fix coding style

--- front/e2e/vue.spec.ts
+++ front/e2e/vue.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
-config({ path: "./.env" });
+config({ path: './.env' });
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  const expectedCopyright = process.env.VITE_APP_COPYRIGHT || 'Application Copyright';
-  await expect(page.locator('#footer-company-copyright')).toContainText(expectedCopyright);
+  const expectedCopyright =
+    process.env.VITE_APP_COPYRIGHT || 'Application Copyright';
+  await expect(page.locator('#footer-company-copyright')).toContainText(
+    expectedCopyright
+  );
 });
--- front/env.d.ts
+++ front/env.d.ts
@@ -7,7 +7,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_COPYRIGHT: string;
   readonly VITE_APP_DEFAULT_THEME: 'dark' | 'light';
   readonly VITE_APP_BASE_URL: string;
-
 }
 
 // eslint-disable-next-line no-unused-vars
