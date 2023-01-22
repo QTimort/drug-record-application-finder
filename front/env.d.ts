26326203c055b9e5108d0aa47005dc1ae0a2d14a
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-22 09:14:39 +0100

    Add Server URL

--- front/env.d.ts
+++ front/env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_COPYRIGHT: string;
   readonly VITE_APP_DEFAULT_THEME: 'dark' | 'light';
   readonly VITE_APP_BASE_URL: string;
+  readonly VITE_APP_SERVER_URL: string;
 }
 
 // eslint-disable-next-line no-unused-vars
--- front/src/config/env.ts
+++ front/src/config/env.ts
@@ -3,7 +3,8 @@ export function getEnvOrDefault(): ImportMetaEnv {
     VITE_APP_TITLE: "Application Name",
     VITE_APP_COPYRIGHT: "© Application Copyright",
     VITE_APP_DEFAULT_THEME: "light",
-    VITE_APP_BASE_URL: "/"
+    VITE_APP_BASE_URL: "/",
+    VITE_APP_SERVER_URL: "http://localhost:8080",
   }
   const env: ImportMetaEnv = import.meta.env;
 
