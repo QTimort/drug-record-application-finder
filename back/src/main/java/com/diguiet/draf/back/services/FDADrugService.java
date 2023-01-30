f8e31488cfc9ac714258575c0208ff667adbbaff
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-30 12:16:37 +0100

    Refactor FDADrugService

--- back/src/main/java/com/diguiet/draf/back/services/FDADrugService.java
+++ back/src/main/java/com/diguiet/draf/back/services/FDADrugService.java
@@ -1,35 +1,44 @@
 package com.diguiet.draf.back.services;
 
 import com.diguiet.draf.common.models.fda.DrugsFdaResponse;
-import lombok.Getter;
-import lombok.NonNull;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import okhttp3.HttpUrl;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class FDADrugService {
-    private static final String DEFAULT_FDA_DRUG_API_ENDPOINT = "https://api.fda.gov/drug/drugsfda.json";
-
     @Getter
-    private final String fdaDrugEndpoint;
+    private final HttpUrl fdaDrugEndpoint;
     private final RestTemplate restTemplate;
+    private final int maxPageSize;
 
-    public FDADrugService(@NonNull final String fdaDrugEndpoint) {
-        this.fdaDrugEndpoint = fdaDrugEndpoint;
+    public FDADrugService(
+            @Value("${fda.api.json}") @NonNull final String fdaDrugEndpoint,
+            @Value("${fda.api.page.maxSize}") final int maxPageSize
+    ) {
+        this.fdaDrugEndpoint = HttpUrl.parse(fdaDrugEndpoint);
+        this.maxPageSize = maxPageSize;
         this.restTemplate = new RestTemplate();
-    }
-
-    public FDADrugService() {
-        this(FDADrugService.DEFAULT_FDA_DRUG_API_ENDPOINT);
     }
 
     public DrugsFdaResponse getByManufacturer(
             @NonNull final String manufacturer
     ) {
+        final URI queryUrl = this.newSearchQueryBuilder()
+                .setManufacturerName(manufacturer, false)
+                .build()
+                .uri();
+
         return this.restTemplate.getForObject(
-                this.fdaDrugEndpoint + "?search=openfda.manufacturer_name:\"{manufacturer}\"&limit=1000",
-                DrugsFdaResponse.class,
-                manufacturer
+                queryUrl,
+                DrugsFdaResponse.class
         );
     }
 
@@ -37,15 +46,104 @@ public class FDADrugService {
             @NonNull final String manufacturer,
             @NonNull final String brand
     ) {
+        final URI queryUrl = this.newSearchQueryBuilder()
+                .setManufacturerName(manufacturer, false)
+                .setBrandNameName(brand, true)
+                .build()
+                .uri();
+
         return this.restTemplate.getForObject(
-                this.fdaDrugEndpoint +
-                        "?search=openfda.manufacturer_name:\"{manufacturer}\"" +
-                        "+AND+" +
-                        "openfda.brand_name.exact:\"{brand}\"&limit=1000",
-                DrugsFdaResponse.class,
-                manufacturer,
-                brand
+                queryUrl,
+                DrugsFdaResponse.class
         );
+    }
+
+    public SearchQueryBuilder newSearchQueryBuilder() {
+        return new SearchQueryBuilder();
+    }
+
+    public class SearchQueryBuilder {
+        private static final String SEARCH_QUERY_PARAMETER_NAME = "search";
+        private static final String LIMIT_QUERY_PARAMETER_NAME = "limit";
+        private static final String MANUFACTURER_NAME_FIELD = "openfda.manufacturer_name";
+        private static final String BRAND_NAME_FIELD = "openfda.brand_name";
+        private static final String EXACT_MATCH_SUFFIX = ".exact";
+        private static final String AND_JOIN_OPERATOR = "\"+AND+\"";
+        private final HttpUrl.Builder urlBuilder;
+        private final int maxLimit;
+
+        private final Map<String, SearchField> searchFields = new HashMap<>();
+        private int limit;
+
+
+        private record SearchField(
+                String name,
+                boolean exact
+        ) {}
+
+        public SearchQueryBuilder(HttpUrl.Builder urlBuilder, int maxLimit) {
+            this.urlBuilder = urlBuilder;
+            this.maxLimit = maxLimit;
+            this.limit = maxLimit;
+        }
+
+        private SearchQueryBuilder() {
+            this(FDADrugService.this.fdaDrugEndpoint.newBuilder(), FDADrugService.this.maxPageSize);
+        }
+
+        public HttpUrl build() {
+            final String searchQueryParameterValue = this.searchFields.entrySet()
+                    .stream()
+                    .map(searchField -> searchFieldQueryToString(searchField.getKey(), searchField.getValue()))
+                    .collect(Collectors.joining(SearchQueryBuilder.AND_JOIN_OPERATOR));
+
+            return this.urlBuilder.setQueryParameter(
+                    SearchQueryBuilder.SEARCH_QUERY_PARAMETER_NAME,
+                    searchQueryParameterValue
+            ).setQueryParameter(
+                    SearchQueryBuilder.LIMIT_QUERY_PARAMETER_NAME,
+                    Integer.toString(this.limit)
+            ).build();
+        }
+
+        public SearchQueryBuilder withLimit(final int limit) {
+            if (limit > this.maxLimit) {
+                throw new IllegalArgumentException("Provided Limit exceed max allowed limit of " + this.maxLimit);
+            }
+            if (limit < 0) {
+                throw new IllegalArgumentException("Provided Limit must be a positive integer");
+            }
+            this.limit = 0;
+            return this;
+        }
+
+        private SearchQueryBuilder setField(
+                @NonNull final String fieldName,
+                @NonNull final String manufacturerName,
+                final boolean exactMatch
+        ) {
+            this.searchFields.put(fieldName, new SearchField(manufacturerName, exactMatch));
+            return this;
+        }
+
+        public SearchQueryBuilder setManufacturerName(@NonNull final String manufacturerName, final boolean exactMatch) {
+            return setField(SearchQueryBuilder.MANUFACTURER_NAME_FIELD, manufacturerName, exactMatch);
+        }
+
+        public SearchQueryBuilder setBrandNameName(@NonNull final String brandName, final boolean exactMatch) {
+            return setField(SearchQueryBuilder.BRAND_NAME_FIELD, brandName, exactMatch);
+        }
+
+        private static String searchFieldQueryToString(
+                @NonNull final String fieldName,
+                @NonNull final FDADrugService.SearchQueryBuilder.SearchField searchField
+        ) {
+            final StringBuilder stringBuilder = new StringBuilder(fieldName);
+            if (searchField.exact) {
+                stringBuilder.append(SearchQueryBuilder.EXACT_MATCH_SUFFIX);
+            }
+            return stringBuilder.append(':').append(searchField.name).toString();
+        }
     }
 
 }
