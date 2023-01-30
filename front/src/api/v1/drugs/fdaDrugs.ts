4caf0e50069e4b7f1206429b2f54bf586897089b
Timothy Diguiet <timothy.diguiet@epitech.eu>
2023-01-30 14:01:44 +0100

    Implement page

--- front/src/api/v1/drugs/fdaDrugs.ts
+++ front/src/api/v1/drugs/fdaDrugs.ts
@@ -4,12 +4,28 @@ import type {DrugsFdaResponse} from "@/types/drafModels";
 /**
  *
  */
-export function getDrugsManufacturer(url: string, manufacturer: string): Promise<DrugsFdaResponse> {
+export function getDrugsManufacturer(
+  url: string, manufacturer: string, page = 1
+): Promise<DrugsFdaResponse> {
   const version = mande(url);
-  return version.get<DrugsFdaResponse>(`/api/v1/drugs/manufacturers/${manufacturer}`);
+  return version.get<DrugsFdaResponse>(`/api/v1/drugs/manufacturers/${manufacturer}`,
+    {
+      query: {
+        page
+      }
+    }
+  );
 }
 
-export function getDrugsManufacturerBrand(url: string, manufacturer: string, brand: string): Promise<DrugsFdaResponse> {
+export function getDrugsManufacturerBrand(
+  url: string, manufacturer: string, brand: string, page = 1
+): Promise<DrugsFdaResponse> {
   const version = mande(url);
-  return version.get<DrugsFdaResponse>(`/api/v1/drugs/manufacturers/${manufacturer}/brands/${brand}`);
+  return version.get<DrugsFdaResponse>(`/api/v1/drugs/manufacturers/${manufacturer}/brands/${brand}`,
+    {
+      query: {
+        page
+      }
+    }
+  );
 }
